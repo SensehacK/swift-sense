79395cf62d0735227df04a246caf2b80f587bd03
Kautilya Save <kautilyasave@gmail.com>
2023-04-14 11:43:56 -0600

    better error for consumer

--- Sources/swift-sense/Webservice.swift
+++ Sources/swift-sense/Webservice.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public enum NetworkError: Error {
     case requestBad
-    case requestDecodingFailed
+    case requestDataFailed
 }
 
 
@@ -21,16 +21,16 @@ public class Webservice {
      Makes a network request using Generic type T: Codable and returns the two completioner handlers - Parsing & Result Type.
     
     Consuming this API is quite easy and it also gives us an option to parse the network data response in the completion handler
-
+     
+     - Parameter url: URL
      - Parameter parse: Call internal parsing closure
      - Parameter completion: Return Result(success, failure) `T:Codable` value
      - Returns: Result<T? ,Error>
      - Warning: Safely unwraping of `T:Codable` is consumer's responsibility.
      
     ```
-     Webservice().fetch(url: url) { data -> [CodableData]? in
-         guard let data: [CodableData] = try? JSONDecoder().decode([CodableData].self, from: data) else { return nil }
-         return data
+     Webservice().fetch(url: url) { data in
+         return try? JSONDecoder().decode([T].self, from: data)
      } completion: { result in
          switch result {
          case .success(let data):
@@ -57,7 +57,57 @@ public class Webservice {
             // Check for data or error in callback
             guard let data = data,
                   error == nil else {
-                completion(.failure(.requestDecodingFailed))
+                completion(.failure(.requestDataFailed))
+                return
+            }
+            
+            let result = parse(data)
+            completion(.success(result))
+        }.resume()
+        
+    }
+    
+    /**
+     Makes a network request using Generic type T: Codable and returns the two completioner handlers - Parsing & Result Type.
+    
+    Consuming this API is quite easy and it also gives us an option to parse the network data response in the completion handler
+     
+     - Parameter urlRequest: URLRequest
+     - Parameter parse: Call internal parsing closure
+     - Parameter completion: Return Result(success, failure) `T:Codable` value
+     - Returns: Result<T? ,Error>
+     - Warning: Safely unwraping of `T:Codable` is consumer's responsibility.
+     
+    ```
+     Webservice().fetch(urlRequest: urlRequest) { data in
+         return try? JSONDecoder().decode([T].self, from: data)
+     } completion: { result in
+         switch result {
+         case .success(let data):
+             guard let data = data else { return }
+             DispatchQueue.main.async {
+                 self.dataStruct = data
+             }
+         case .failure(let error):
+             print(error)
+         }
+     }
+    ```
+    */
+    public func fetch<T: Codable>(urlRequest: URLRequest, parse: @escaping (Data) -> T?,
+                                  completion: @escaping (Result<T?, NetworkError>) -> Void)  {
+        
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            guard let response = response as? HTTPURLResponse,
+                  response.isResponseOK() else {
+                completion(.failure(.requestBad))
+                return
+            }
+
+            // Check for data or error in callback
+            guard let data = data,
+                  error == nil else {
+                completion(.failure(.requestDataFailed))
                 return
             }
             
