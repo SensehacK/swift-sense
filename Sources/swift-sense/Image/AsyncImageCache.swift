de1c98477c0958985bfdf1dc65d3e293cd55c777
Kautilya Save <kautilyasave@gmail.com>
2023-04-19 13:25:02 -0600

    Image: cache using Dictionary

---
+++ Sources/swift-sense/Image/AsyncImageCache.swift
@@ -0,0 +1,47 @@
+//
+//  AsyncImageCache.swift
+//  
+//
+//  Created by Kautilya Save on 4/19/23.
+//
+
+import SwiftUI
+
+@available(iOS 15.0, *)
+struct AsyncImageCache<T>: View where T: View {
+    
+    private let url: URL
+    private let key: String
+    private let content: (AsyncImagePhase) -> T
+    
+    init(url: URL,
+         key: String = UUID().uuidString,
+         @ViewBuilder content: @escaping (AsyncImagePhase) -> T) {
+        self.url = url
+        self.key = key
+        self.content = content
+    }
+    
+    var body: some View {
+        
+        // Check for Cache Dictionary first
+        if let cachedImage = CacheDictionary[key] {
+            content(.success(cachedImage))
+        } else {
+            // Or else return the normal Network fetch image cache
+            AsyncImage(url: url) { imagePhase in
+                returnSuccessfulImageAndCache(phase: imagePhase)
+            }
+        }
+
+    }
+    
+    
+    func returnSuccessfulImageAndCache(phase: AsyncImagePhase) -> some View {
+        if case .success(let image) = phase {
+            CacheDictionary[key] = image
+        }
+        return content(phase)
+    }
+    
+}
